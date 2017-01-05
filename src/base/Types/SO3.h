5ed0659909ae75700c3eea06908354af614cce5d
zhaoyong <zd5945@126.com>
2017-01-05 14:10:01 +0800

    fixed bug of SO3::fromMatrix(const Precision* m)

--- src/base/Types/SO3.h
+++ src/base/Types/SO3.h
@@ -158,12 +158,68 @@ public:
     /// |m6 m7 m8|
     inline bool fromMatrix(const Precision* m)
     {
-        w=0.5*sqrt(m[0]+m[4]+m[8]+1);
-        Precision oneover4w=0.25/w;
-        x=(m[7]-m[5])*oneover4w;
-        y=(m[2]-m[6])*oneover4w;
-        z=(m[3]-m[1])*oneover4w;
-        if(w<0){x*=-1;y*=-1;z*=-1;w*=-1;}
+        if(false)
+        {
+            Precision tr=m[0]+m[4]+m[8];
+            w=0.5*sqrt(tr+1);
+            Precision oneover4w=0.25/w;
+            x=(m[7]-m[5])*oneover4w;
+            y=(m[2]-m[6])*oneover4w;
+            z=(m[3]-m[1])*oneover4w;
+            if(w<0){x*=-1;y*=-1;z*=-1;w*=-1;}
+            return true;
+        }
+        else
+        {
+            Precision& q0=w;
+            Precision& q1=x;
+            Precision& q2=y;
+            Precision& q3=z;
+            const Precision &r11=m[0],&r12=m[1],&r13=m[2],
+                    &r21=m[3],&r22=m[4],&r23=m[5],
+                    &r31=m[6],&r32=m[7],&r33=m[8];
+            q0 = ( r11 + r22 + r33 + 1.0f) / 4.0f;
+            q1 = ( r11 - r22 - r33 + 1.0f) / 4.0f;
+            q2 = (-r11 + r22 - r33 + 1.0f) / 4.0f;
+            q3 = (-r11 - r22 + r33 + 1.0f) / 4.0f;
+            if(q0 < 0.0f) q0 = 0.0f;
+            if(q1 < 0.0f) q1 = 0.0f;
+            if(q2 < 0.0f) q2 = 0.0f;
+            if(q3 < 0.0f) q3 = 0.0f;
+            q0 = sqrt(q0);
+            q1 = sqrt(q1);
+            q2 = sqrt(q2);
+            q3 = sqrt(q3);
+            if(q0 >= q1 && q0 >= q2 && q0 >= q3) {
+                q0 *= +1.0f;
+                q1 *= SIGN(r32 - r23);
+                q2 *= SIGN(r13 - r31);
+                q3 *= SIGN(r21 - r12);
+            } else if(q1 >= q0 && q1 >= q2 && q1 >= q3) {
+                q0 *= SIGN(r32 - r23);
+                q1 *= +1.0f;
+                q2 *= SIGN(r21 + r12);
+                q3 *= SIGN(r13 + r31);
+            } else if(q2 >= q0 && q2 >= q1 && q2 >= q3) {
+                q0 *= SIGN(r13 - r31);
+                q1 *= SIGN(r21 + r12);
+                q2 *= +1.0f;
+                q3 *= SIGN(r32 + r23);
+            } else if(q3 >= q0 && q3 >= q1 && q3 >= q2) {
+                q0 *= SIGN(r21 - r12);
+                q1 *= SIGN(r31 + r13);
+                q2 *= SIGN(r32 + r23);
+                q3 *= +1.0f;
+            } else {
+                printf("coding error\n");
+                return false;
+            }
+            Precision r = NORM(q0, q1, q2, q3);
+            q0 /= r;
+            q1 /= r;
+            q2 /= r;
+            q3 /= r;
+        }
         return true;
     }
 
